4a08640b3e432f4e821ea8e9f44f6fd5a7b88712
zirui-wang <zirui.zirui.wang.wang@gmail.com>
2018-06-19 20:52:15 -0700

    Add one more solution for [102. Binary Tree Level Order Traversal]

--- LeetCode/102_Binary_Tree_Level_Order_Traversal.java
+++ LeetCode/102_Binary_Tree_Level_Order_Traversal.java
@@ -1,8 +1,11 @@
 /**
+ * Solution 1: 时间复杂度n, 空间复杂度n
  * 1. 利用queue先进先出的特性，保持遍历的先后顺序。
  * 2. 每层循环的长度用size = queue.size()确定。
  * 
- * Solution 1: 时间复杂度n, 空间复杂度n
+ * Solution 2: 时间复杂度n, 空间复杂度n
+ * 1. inorder递归，用Map记录每一层的List。
+ * 2. 结束递归，for循环讲map中的list放入result。
  */
 
 /**Solution 1 */
@@ -30,5 +33,36 @@ class Solution {
             result.add(subList);
         }
         return result;
+    }
+}
+
+/**Solution 2 */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null){
+            return result;
+        }
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        helper(root, 0, map);
+        for(int key : map.keySet()){
+            result.add(map.get(key));
+        }
+        return result;
+    }
+    
+    private void helper(TreeNode node, int level, Map<Integer, List<Integer>> map){
+        if(node == null){
+            return;
+        }
+        helper(node.left, level + 1, map);
+        if(map.containsKey(level)){
+            map.get(level).add(node.val);
+        }else{
+            List<Integer> record = new ArrayList<>();
+            record.add(node.val);
+            map.put(level, record);
+        }
+        helper(node.right, level + 1, map);
     }
 }
