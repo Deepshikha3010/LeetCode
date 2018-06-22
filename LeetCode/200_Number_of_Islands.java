6ebb5bfa414a2cca9e56d471d01b8702dd262a32
ZiruiWang <32398353+zirui-wang@users.noreply.github.com>
2018-06-22 14:39:47 -0700

    Update 200_Number_of_Islands.java

--- LeetCode/200_Number_of_Islands.java
+++ LeetCode/200_Number_of_Islands.java
@@ -5,6 +5,7 @@
  * 时间复杂度mn，空间复杂度mn
  */
 
+/**Solution 1 Iteration + Point class */
 class Solution {
     private class Point{
         int x;
@@ -54,5 +55,43 @@ class Solution {
         int height = grid.length;
         int width = grid[0].length;
         return p.x >= 0 && p.x < height && p.y >= 0 && p.y < width;
+    }
+}
+
+/**Solution 2 Recursion */
+class Solution {
+    public int numIslands(char[][] grid) {
+        if(grid == null || grid.length == 0 || grid[0].length == 0){
+            return 0;
+        }
+        int m = grid.length;
+        int n = grid[0].length;
+        int result = 0;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == '1'){
+                    helper(grid, i, j);
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+    
+    private void helper(char[][] grid, int x, int y){
+        int[] xCord = new int[]{-1, 0, 1, 0};
+        int[] yCord = new int[]{0, -1, 0, 1};
+        grid[x][y] = '0';
+        for(int i = 0; i < xCord.length; i++){
+            int m = x + xCord[i];
+            int n = y + yCord[i];
+            if(isInBound(grid, m, n) && grid[m][n] == '1'){
+                helper(grid, m, n);
+            }
+        }
+    }
+    
+    private boolean isInBound(char[][] grid, int i, int j){
+        return i >= 0 && i < grid.length && j >= 0 && j < grid[0].length;
     }
 }
