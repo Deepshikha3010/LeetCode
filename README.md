73b6b9f498b67c0a2921472cc879633235c78a84
zirui-wang <zirui.zirui.wang.wang@gmail.com>
2018-12-17 13:42:23 -0800

    Add 684.

--- README.md
+++ README.md
@@ -56,6 +56,7 @@ Algorithm and data structure solutions in Java 8.
 |[515. Find Largest Value in Each Tree Row](./Solutions/515_Find_Largest_Value_in_Each_Tree_Row/README.md)|Medium|
 |[529. Minesweeper](./Solutions/529_Minesweeper/README.md)|Medium|
 |[542. 01 Matrix](./Solutions/542_01_Matrix/README.md)|Medium|
+|[684. Redundant Connection](./Solutions/684_Redundant_Connection/README.md)|Medium|
 |[690. Employee Importance](./Solutions/690_Employee_Importance/README.md)|Easy|
 |[773. Sliding Puzzle](./Solutions/773_Sliding_Puzzle/README.md)|Hard|
 |[787. Cheapest Flights Within K Stops](./Solutions/787_Cheapest_Flights_Within_K_Stops/README.md)|Medium|
---
+++ Solutions/684_Redundant_Connection/Solution1.java
@@ -0,0 +1,49 @@
+/**
+ * Type: Union Find
+ * Time: n
+ * Space: n
+ */
+class Solution {
+    private int[] parents;
+    
+    private int find(int x) {
+        if (x == parents[x]) return x;
+        return parents[x] = find(parents[x]);
+    }
+    
+    public int[] findRedundantConnection(int[][] edges) {
+        if (edges == null || edges.length == 0) return new int[]{-1, -1};
+        
+        Set<Integer> set = new HashSet<>();
+        for (int[] edge : edges) {
+            set.add(edge[0]);
+            set.add(edge[1]);
+        }
+    
+        int n = set.size();
+
+        // Time: n
+        // Space: n
+        parents = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parents[i] = i;
+        }
+        
+        int[] res = new int[]{-1, -1};
+        
+        // Connect
+        // Time: n
+        for (int[] edge : edges) {
+            int root1 = find(edge[0]);
+            int root2 = find(edge[1]);
+            
+            if (root1 != root2) {
+                parents[root1] = root2;
+            } else {
+                res = edge;   
+            }
+        }
+        
+        return res;
+    }
+}
